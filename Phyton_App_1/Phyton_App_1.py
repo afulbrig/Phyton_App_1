9d86b17f3fa2a0d98f815c942915cdfe8facc9d9
Alex Fulbright <afulbrig@gmail.com>
2025-11-04 12:30:03 -0600

    Add project files.

---
+++ Phyton_App_1/Phyton_App_1.py
@@ -0,0 +1,46 @@
+# My first Python program
+# The is a coment - Python skips this line
+
+print("Hello, world.")
+
+
+print("Hello, Alex! Welcome to Python.") # I can also comment at the end of a line
+name = "Alex"
+age = 35
+hobby = "gaming"
+print(f"My name is {name} and I'm {age} years old. My favorite hobby is {hobby}.")
+
+# I can change a variable value later
+
+hobby = "cooking"
+print(f"My name is {name} and I'm {age} years old.  My favorite hobby is {hobby}")
+
+
+
+"""
+This is a multi-line comment.
+It is useful for describing large sections of code.
+"""
+
+user_name = "Alex"
+UserName = "Henry"
+
+print(f"My name is {user_name} and I am learning Python with {UserName}.")
+
+# I can even do math or logic in line
+
+print(f"In 10 years, I'll be {age + 10}.")
+
+
+"""
+Practice Assignment
+Temperature Converter
+"""
+
+celsius = 25
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"It is {celsius} C outside.")
+
+print(f"It is {fahrenheit} F outside.")
+
